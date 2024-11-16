dedcbfcac9600383e1ba52a51a99d253abe1dbcf
keith <keith@keiths-iMac.local>
2024-11-16 11:29:48 -0500

    Tests.

--- app/lib/verify-address.spec.ts
+++ app/lib/verify-address.spec.ts
@@ -1,4 +1,4 @@
-import { verifyXpub } from './verify-address';
+import { verifyXpub, getPrefix } from './verify-address';
 
 describe('verifyXpub', () => {
 
@@ -37,9 +37,53 @@ describe('verifyXpub', () => {
         expect(verifyXpub(valid)).toBe(true);
     });
 
+    test('Testnet garbage', () => {
+        const invalid = 'tpubDCdJV3Q5vPhUmKiSCiS68w43yKZKEZsd8wxa3t8q4DsPjfejaPDT76SDcf6iscyvLnpz4UXFMzVXxZmH5U6XEQfQYGit';
+        expect(verifyXpub(invalid)).toBe(false);
+    });
+
     test('ypub', () => {
     });
 
     test('zpub', () => {
+    });
+});
+
+
+describe('getPrefix', () => {
+
+    test('1', () => {
+        const address = '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa';
+        expect(getPrefix(address)).toBe("1");
+    });
+
+    test('3', () => {
+        const address = '3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy';
+        expect(getPrefix(address)).toBe("3");
+    });
+
+    test('bc1q', () => {
+        const address = 'bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kygt080';
+        expect(getPrefix(address)).toBe("bc1q");
+    });
+
+    test('bc1p', () => {
+        const address = 'bc1p5d7rjq7g6rdk2yhzks9smlaqtedr4dekq08ge8ztwac72sfr9rusxg3297';
+        expect(getPrefix(address)).toBe("bc1p");
+    });
+
+    test('tb1q', () => {
+        const address = 'tb1q6l2jt0ysay05yg2af0zx87p06vdyf4rhcp6npe';
+        expect(getPrefix(address)).toBe("tb1q");
+    });
+
+    test('tb1p', () => {
+        const address = 'tb1p6rgz84xkrn8l96s7vx6jd7xae34gs22c9a20q5p86esfj4qr3c0q3k5jjs';
+        expect(getPrefix(address)).toBe("tb1p");
+    });
+
+    test('garbage', () => {
+        const address = 'garbage';
+        expect(getPrefix(address)).toBe("");
     });
 });
