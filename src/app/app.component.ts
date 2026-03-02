2c437a886bf011c42c89af7d2c869e2817a1648f
Kyle McCullen <kmccullen@protonmail.com>
2026-03-02 07:44:10 -0500

    add disclaimer to first time running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
 import { RouterOutlet } from '@angular/router';
 import { version } from '../../package.json';
+import { ConfirmationDialogComponent } from './shared/components/confirmation-dialog/confirmation-dialog.component';
 import { NavigationComponent } from './core/navigation/navigation.component';
+import { SettingsService } from './shared/services/settings.service';
 import { environment } from '../environments/environment';
 
 @Component({
@@ -12,13 +15,36 @@ import { environment } from '../environments/environment';
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     version: string = version + (environment.name == 'production' ? '' : '-dev');
 
     titleBarClass: string = "";
 
+    private dialog = inject(MatDialog);
+    private settingsService = inject(SettingsService);
+
     constructor() {
         this.titleBarClass = window.electron.platform();
+    }
+
+    ngOnInit(): void {
+        if (!this.settingsService.settings().disclaimerAccepted) {
+            const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+                disableClose: true,
+                data: {
+                    title: 'Welcome to Branta Core',
+                    message: 'Branta Core is free, open source software under the MIT license. By using this software, you acknowledge this.',
+                    submitText: 'I Acknowledge',
+                    hideCancel: true
+                }
+            });
+
+            dialogRef.afterClosed().subscribe((result) => {
+                if (result === true) {
+                    this.settingsService.save({ ...this.settingsService.settings(), disclaimerAccepted: true });
+                }
+            });
+        }
     }
 
     onHelp(): void {
